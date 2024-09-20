a8cf7cd63bf51e7e4528e1beb57acb02d33d0375
destroyerman <destroyerman.dev@proton.me>
2024-09-20 12:30:11 +0800

    add meta tag and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,12 @@
 	import ComingSoon from '../lib/components/global/ComingSoon.svelte';
 </script>
 
+<svelte:head>
+	<title>Coming Soon</title>
+	<meta
+		name="description"
+		content="Marguerite is a top-tier web agency specializing in web design, development, and SEO. We help companies, independent businesses, and entrepreneurs scale with high-converting websites that drive real results. Ready to take your business to the next level? Let's build a digital presence that delivers serious ROI."
+	/>
+</svelte:head>
+
 <ComingSoon />
